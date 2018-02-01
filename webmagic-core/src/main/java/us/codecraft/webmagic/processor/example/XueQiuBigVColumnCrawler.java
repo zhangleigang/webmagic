89aa3a678cb44be8d157ec59657a675fb3b603dc
zhangleigang <zhangleigang@jd.com>
2018-02-01 21:27:22 +0800

    href开关

--- webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/XueQiuBigVColumnCrawler.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/XueQiuBigVColumnCrawler.java
@@ -17,10 +17,11 @@ public class XueQiuBigVColumnCrawler {
 				.addCookie("xq_a_token", "3de6f1ebe7a2e5e316e7a47eaf8df0610e5909c5")
 				.addCookie("xq_is_login", "1")
 				.addCookie("xq_r_token", "f8b423252994c7922fdbb68e9989e10b3b50b2dd")
-				.addCookie("xq_token_expire", "Tue%20Feb%2013%202018%2014%3A58%3A37%20GMT%2B0800%20(CST)"), 
+				.addCookie("xq_token_expire", "Tue%20Feb%2013%202018%2014%3A58%3A37%20GMT%2B0800%20(CST)"),
 				 new XueQiuBigVColumnDaoPipeline(),
 				 XueQiuBigVColumnModel.class)
-		        .addUrl("https://xueqiu.com/statuses/original/timeline.json?user_id=1955602780&page=2")
+		        .addUrl("https://xueqiu.com/statuses/original/timeline.json?user_id=1955602780&page=1")
+		        //.setSpawnUrl(false)
 		        .thread(1).run();
 	}
 }
